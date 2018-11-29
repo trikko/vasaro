ade3c59cc585d5203a9c8cf5366cbfb54b487b5d
Andrea Fontana <trikkuz@gmail.com>
2018-11-29 23:03:36 +0100

    Adding demo.gif

---
+++ createMacOSBundle.sh
@@ -0,0 +1,1 @@
+rm -rf /tmp/vasaro && mkdir /tmp/vasaro && cp -R deployment/osx/vasaro.app /tmp/vasaro && mkdir -p /tmp/vasaro/vasaro.app/Resources/MacOS && cp vasaro /tmp/vasaro/vasaro.app/Resources/MacOS/ && ln -s /Applications /tmp/vasaro/Applications && hdiutil create -volname vasaro -srcfolder /tmp/vasaro -ov -format UDZO /tmp/vasaro.dmg && open /tmp/vasaro.dmg"
